4d8edda9021324a093c521b127cdf4d95215b5ad
Karanjot Singh <karanjotsingh@Karanjots-MacBook-Air.local>
2021-08-31 16:00:39 -0500

    completed

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="d-flex flex-row"> 
       <button onClick={()=>setshowMenu(!showMenu)} className="d-sm-block d-md-none menu-button">|||</button>
-      <div className={`col-md-3 menu d-md-block ${showMenu? 'd-block':'d-none'}`}>
+      <div className={`col-md-3 p-md-3 menu d-md-block ${showMenu? 'd-block':'d-none'}`}>
         <Sidebar/>
       </div>
       <div className="col-md-9 col-12 container">
